a83ebe16413a941a8be661bf23cdeafa0154a247
Shivam Mishra <scm.mymail@gmail.com>
2018-03-23 16:23:17 +0530

    Mother registration complete

--- src/app/mother.model.ts
+++ src/app/mother.model.ts
@@ -2,12 +2,14 @@ export class Mother {
     constructor(
         public first_name: string,
         public last_name: string,
-        public rch_id: string,
-        public location: string,
-        public mobile: string,
+        public mobile1: string,
+        public mobile2: string,
         public date_of_birth: string,
+        public aadhar: string,
+        public registration_date: string,
         public blood_group: string,
-        public delivery_complication: string,
-        public education: string
+        public economic_status: string,
+        public education: string,
+        public hiv_status: string,
       ) { }
 }
