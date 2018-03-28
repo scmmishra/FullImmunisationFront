b041e36464a7fddeb260a2bf2b7540b1e1faccb9
RnabSanyal <=>
2018-03-28 15:31:42 +0530

    Immunization Search Done | Submit remaining

--- src/app/Fields.ts
+++ src/app/Fields.ts
@@ -1,7 +1,11 @@
 export class Fields{
 	constructor(
-		id: number,
-		first_name: string,
-		last_name: string
+		public mother: number,
+		public first_name: string,
+		public date_of_birth: string,
+		public blood_group: string,
+		public gender: string,
+		public hiv_status: string,
+		public registration_date: string,
      ) { }
 }
--- src/app/immunization/immunization.component.spec.ts
+++ src/app/immunization/immunization.component.spec.ts
@@ -23,3 +23,4 @@ describe('ImmunizationComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
